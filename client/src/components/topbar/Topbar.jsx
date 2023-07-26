250822651f4272cceccffcc0feaf6353ccebd860
Kunalhume <kunal.hume33@gmail.com>
2023-07-26 15:40:30 +0530

    Updates

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,42 +1,56 @@
 import "./topbar.css";
-import {Chat, Notifications, Person, Search} from '@mui/icons-material';
+import { Chat, Notifications, Person, Search } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
     <div className="topbarContainer">
-      <div className="topbarLeft">
-        <span className="logo">K & D</span>
-      </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
-          <Search className="searchIcon"/>
-          <input type="text" className="searchInput" placeholder="Search for friend, post or video" />
+      <div className="topbarInnerContainer">
+        <div className="topbarLeft">
+          <span className="logo">K & D</span>
         </div>
-      </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
-          <span className="topbarLink"><Link to='/' className="link">Home</Link></span>
-          <span className="topbarLink">Timeline</span>
-        </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person/>
-            <span className="topbarIconBadge">1</span>
-          </div>
-          <div className="topbarIconItem">
-            <Chat/>
-            <span className="topbarIconBadge">2</span>
-          </div>
-          <div className="topbarIconItem">
-            <Notifications/>
-            <span className="topbarIconBadge">1</span>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input
+              type="text"
+              className="searchInput"
+              placeholder="Search for friend, post or video"
+            />
           </div>
         </div>
-        <img  className="topbarImg" src="/assets/person/3.jpg" alt="Profilepicture" />
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">2</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </div>
+          <img
+            className="topbarImg"
+            src="/assets/person/3.jpg"
+            alt="Profilepicture"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
